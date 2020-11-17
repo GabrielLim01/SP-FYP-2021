df1d78e6660a1db0ee078be24790f73408a35ce5
Wei Xian <wxian20@gmail.com>
2020-11-17 16:52:45 +0800

    Getting rid of redundant stuffs, add concurrently

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => { 
+    const text = 'Hello World from server.js';
+    res.json(text);
+});
+
+const port = 9000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
