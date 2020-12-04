c982f9f03e5d9c8f5e2932c5df533bc1a48b082e
Wei Xian <wxian20@gmail.com>
2020-12-04 22:50:49 +0800

    WIP

--- db.js
+++ db.js
@@ -1,28 +1,81 @@
-const mysql = require('mysql');
-const dotenv = require('dotenv');
+const mysql = require("mysql");
+const dotenv = require("dotenv");
 let instance = null;
 dotenv.config();
 
 // Create Database Connection
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.MySQL_DB
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.MySQL_DB,
+  port: 3306,
 });
 
 // To Connect
-db.connect((err) => { 
-    if (err) { 
-        throw err;
-    }
-    console.log('Connection Successful!');
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connection Successful!");
 });
 
 class DbService {
-    static getDbServiceInstance() {
-        return instance ? instance : new DbService();
+  static getDbServiceInstance() {
+    return instance ? instance : new DbService();
+  }
+
+  async getAllUsers() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM users;";
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  async insertNewUser(name, pwd) {
+    try {
+      const insertId = await new Promise((resolve, reject) => {
+        const query =
+          "INSERT INTO users (name, password, insertId) VALUES (?,?,0);";
+
+        connection.query(query, [name, pwd], (err, result) => {
+          if (err) reject(new Error(err.message));
+        });
+      });
+      return {
+        name: name,
+        password: pwd,
+        insertId: insertId,
+      };
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async get(name) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM users WHERE name=?;";
+
+        connection.query(query, [name], (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 module.exports = DbService;
--- server.js
+++ server.js
@@ -1,22 +1,52 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const cors = require('cors');
-const dotenv = require('dotenv');
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const dbService = require('./db');
+const dbService = require("./db");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended : false }));
-
-app.get('/', (req, res) => { 
-    const text = 'Hello World from server.js';
-    res.json(text);
-});
+app.use(express.urlencoded({ extended: true }));
 
 const port = 9000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// read
+app.get("/getAll", (request, response) => {
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.getAllUsers();
+
+  result
+    .then((data) => response.json({ data: data }))
+    .catch((err) => console.log(err));
+});
+
+// Create
+app.post("/users", async (req, res) => {
+  try {
+    const name = req.body.name;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    console.log(hashedPassword);
+    console.log(salt);
+
+    //const pwd = req.body.password;
+
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.insertNewUser(name, hashedPassword);
+
+    result
+      .then((data) => res.json({ data: data }))
+      .catch((err) => console.log(err));
+  } catch {
+    res.status(500).send();
+  }
+});
