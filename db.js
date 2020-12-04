69dabf997fb95b62f06c6d032511ac41d2253f91
Wei Xian <wxian20@gmail.com>
2020-12-05 01:18:47 +0800

    WIP

--- db.js
+++ db.js
@@ -1,28 +1,115 @@
-const mysql = require('mysql');
-const dotenv = require('dotenv');
+const mysql = require("mysql");
+const dotenv = require("dotenv");
 let instance = null;
 dotenv.config();
 
 // Create Database Connection
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.MySQL_DB
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.MySQL_DB,
 });
 
 // To Connect
-db.connect((err) => { 
-    if (err) { 
-        throw err;
-    }
-    console.log('Connection Successful!');
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connection Successful!");
 });
 
 class DbService {
-    static getDbServiceInstance() {
-        return instance ? instance : new DbService();
+  static getDbServiceInstance() {
+    return instance ? instance : new DbService();
+  }
+
+  async getAllQuizzes() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM quiz;";
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      // console.log(response);
+      return response;
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  async createQuiz(title, desc, fiqPoints, categoryId) {
+    try {
+      const insertId = await new Promise((resolve, reject) => {
+        const query =
+          "INSERT INTO quiz (categoryId, quizId, quizName, quizDesc, fiqPoints) VALUES (?,0,?,?,?);";
+
+        connection.query(
+          query,
+          [categoryId, title, desc, fiqPoints],
+          (err, result) => {
+            if (err) reject(new Error(err.message));
+            resolve(result.insertId);
+          }
+        );
+      });
+      return {
+        insertId: insertId,
+        categoryid: categoryId,
+        name: title,
+        description: desc,
+        FIQ_Points: fiqPoints,
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async updateDetailsById(id, title, desc, fiqPoints, categoryId) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query =
+          "UPDATE quiz SET categoryId= ?, quizName = ?, quizDesc = ?, fiqPoints = ?  WHERE quizId = ?";
+
+        connection.query(
+          query,
+          [categoryId, title, desc, fiqPoints, id],
+          (err, result) => {
+            if (err) reject(new Error(err.message));
+            resolve(result.affectedRows);
+          }
+        );
+      });
+
+      return response === 1 ? true : false;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async deleteQuizById(id) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "DELETE FROM quiz WHERE quizId = ?";
+
+        connection.query(query, [id], (err, result) => {
+          if (err) reject(new Error(err.message));
+          resolve(result.affectedRows);
+        });
+      });
+
+      return response === 1 ? true : false;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
 }
 
 module.exports = DbService;
--- server.js
+++ server.js
@@ -1,22 +1,80 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const cors = require('cors');
-const dotenv = require('dotenv');
+const cors = require("cors");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const dbService = require('./db');
+const dbService = require("./db");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended : false }));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => { 
-    const text = 'Hello World from server.js';
-    res.json(text);
+app.get("/", (req, res) => {
+  const text = "Hello World from server.js";
+  res.json(text);
 });
 
 const port = 9000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// read
+app.get("/quizDashboard", (request, response) => {
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.getAllQuizzes();
+
+  result
+    .then((data) => response.json({ data: data }))
+    .catch((err) => console.log(err));
+});
+
+// create
+app.post("/createNew", (request, response) => {
+  const title = request.body.title;
+  const desc = request.body.desc;
+  const fiqPoints = request.body.fiqPoints;
+  const categoryId = request.body.categoryId;
+
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.createQuiz(title, desc, fiqPoints, categoryId);
+
+  result
+    .then((data) => response.json({ data: data }))
+    .catch((err) => console.log(err));
+});
+
+// update
+app.patch("/update/:id", (request, response) => {
+  const { id } = request.params;
+
+  const title = request.body.title;
+  const desc = request.body.desc;
+  const fiqPoints = request.body.fiqPoints;
+  const categoryId = request.body.categoryId;
+
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.updateDetailsById(id, title, desc, fiqPoints, categoryId);
+
+  result
+    .then((data) => response.json({ success: data }))
+    .catch((err) => console.log(err));
+});
+
+// delete
+app.delete("/delete/:id", (request, response) => {
+  const { id } = request.params;
+
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.deleteQuizById(id);
+
+  result
+    .then((data) => response.json({ success: data }))
+    .catch((err) => console.log(err));
+});
