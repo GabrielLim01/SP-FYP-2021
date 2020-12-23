d8309a4a5d3bd1b6cf2fd05da2f8175a4753ceb3
Gabriel-Lim01 <gabriellim01.18@ichat.sp.edu.sg>
2020-12-23 19:35:27 +0800

    merging routes.js

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -6,30 +6,24 @@ import Login from './Login.js';
 import Registration from './Registration.js';
 import Dashboard from './Dashboard.js';
 import NotFound from './NotFound.js';
-<<<<<<< HEAD:client/src/components/Routes.js
 
 // Quizzes
 import CategorySelection from './quiz/CategorySelection.js';
 import QuizSelection from './quiz/QuizSelection.js';
 import QuizCreation from './quiz/QuizCreation.js';
-=======
->>>>>>> dc7724fdd136d56a2e18b60b5f9fa4c504aa7360:client/src/components/Main.js
 
 const Routes = () => {
   return (
-    <Switch> 
+    <Switch>
       <Route exact path='/' component={Login}></Route>
       <Route exact path='/register' component={Registration}></Route>
       <Route exact path='/dashboard' component={Dashboard}></Route>
 
-<<<<<<< HEAD:client/src/components/Routes.js
       <Route exact path='/quizzes' component={CategorySelection}></Route>
       <Route exact path='/quizzes/creation' component={QuizCreation}></Route>
       <Route exact path="/quizzes/:categoryName" component={QuizSelection}></Route>
 
-=======
->>>>>>> dc7724fdd136d56a2e18b60b5f9fa4c504aa7360:client/src/components/Main.js
-      <Route component={NotFound} /> 
+      <Route component={NotFound} />
     </Switch>
   );
   // If you prefer to redirect the user to homepage immediately (without displaying an error page), 
