55ad2dc90053c2ba3265359d5791807c42dac996
Wei Xian <wxian20@gmail.com>
2021-01-11 21:45:21 +0800

    WIP

--- client/src/components/quest/questCreation.js
+++ client/src/components/quest/questCreation.js
@@ -1,10 +1,11 @@
 import React from 'react';
 //import { Redirect } from 'react-router-dom';
-import { Button, Dropdown, Popup, Icon } from 'semantic-ui-react';
-import { containerStyle } from '../../common.js';
+import { Button, Dropdown, Icon, Segment, Form, Grid, TextArea } from 'semantic-ui-react';
 import DashboardMenu from '../DashboardMenu.js';
 import QuestScenarioCreation from './questScenarioCreation.js';
 import retrieveItems from '../quiz/retrieveItems.js';
+import axios from 'axios';
+import { host } from '../../common.js';
 
 class QuestCreation extends React.Component {
     constructor(props) {
@@ -28,13 +29,49 @@ class QuestCreation extends React.Component {
         }
     };
 
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
     handleDropdownChange = (event, data) => {
         this.setState({
             [data.name]: data.value,
         });
     };
 
-    handleSubmit = (event) => {};
+    handleSubmit = (event) => {
+        event.preventDefault();
+        let counter = 1;
+        let scenario = [];
+
+        do {
+            const options = [];
+            for (let i = 1; i < this.state.options + 1; i++) {
+                options.push({
+                    name: this.state[`option-${counter}-${i}`],
+                    addOns: this.state[`desc-${counter}-${i}`],
+                });
+            }
+            scenario.push({
+                scenario: this.state[`scenario${counter}name`],
+                options,
+            });
+
+            counter++;
+        } while (counter < this.state.scenario + 1);
+
+        let questObj = {
+            title: this.state.questTitle,
+            desc: this.state.questDesc,
+            categoryId: this.state.questCategory,
+            fiqPoints: this.state.questPoints,
+            scenarios: scenario,
+        };
+
+        axios.post(`${host}/something`, { quest: questObj });
+    };
 
     generateItems() {
         retrieveItems(`category`)
@@ -77,68 +114,80 @@ class QuestCreation extends React.Component {
                     scenarioNumber={i}
                     options={this.state.options}
                     handleChange={this.handleChange}
-                    //handleDropdownChange={this.handleDropdownChange}
+                    handleDropdownChange={this.handleDropdownChange}
                 />,
             );
         }
         return (
-            <div className="container" style={{ textAlign: 'left' }}>
+            <div className="container">
                 <DashboardMenu page="quests"></DashboardMenu>
-                <div className="subContainer" style={containerStyle}>
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <h1>Create New Quest</h1>
-                    </div>
-                    <div className="ui stacked segment">
-                        <form className="ui form">
-                            <div className="equal width fields">
-                                <div className="field">
-                                    <h3>Title</h3>
-                                    <div className="ui fluid input">
-                                        <input type="text" placeholder="Title" />
-                                    </div>
-
-                                    <Popup
-                                        content="How much FIQ (Financial IQ) points should players earn upon completion of the quest?"
-                                        trigger={<h3>Fiq Points</h3>}
-                                    />
-                                    <div className="ui fluid input">
-                                        <input type="text" placeholder="Fiq Points" />
-                                    </div>
-
-                                    <h3>Category</h3>
-                                    <Dropdown
-                                        name="Category"
-                                        placeholder="Select Option"
-                                        fluid
-                                        selection
-                                        options={categoryItems}
-                                        onChange={this.handleDropdownChange}
-                                    />
-                                </div>
-                                <div className="field">
-                                    <h3>Description / Objective</h3>
-                                    <div className="ui fluid input">
-                                        <textarea type="text" placeholder="Description / Objective" />
-                                    </div>
-                                </div>
-                            </div>
-                            {scenarios}
-                            <div className="subContainer" style={{ display: 'flex', flexDirection: 'row-reverse' }}>
-                                <Button className="blue" name="createQuiz" onClick={this.handleSubmit}>
-                                    Create Quest
-                                </Button>
-                                <Button
-                                    icon
-                                    labelPosition="left"
-                                    className="teal"
-                                    name="addScenario"
-                                    onClick={this.onAddScenario}
-                                >
-                                    <Icon name="add" size="large" />
-                                    Add Scenario
-                                </Button>
-                            </div>
-                        </form>
+                <h1 className="ui teal image header">Create your quest!</h1>
+                <div
+                    className="subContainer"
+                    style={{ maxWidth: '70%', margin: 'auto', textAlign: 'left', paddingTop: '20px' }}
+                >
+                    <Segment>
+                        <Form>
+                            <Grid columns="equal">
+                                <Grid.Row columns={2}>
+                                    <Grid.Column>
+                                        <h3>Quest Title</h3>
+                                        <input
+                                            type="text"
+                                            name="questTitle"
+                                            placeholder="Title"
+                                            onChange={this.handleChange}
+                                        />
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <h3>Quest Description</h3>
+                                        <TextArea
+                                            name="questDesc"
+                                            placeholder="Description"
+                                            onChange={this.handleChange}
+                                        />
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row columns={3}>
+                                    <Grid.Column>
+                                        <h3>Category</h3>
+                                        <Dropdown
+                                            name="questCategory"
+                                            placeholder="Select a Category"
+                                            fluid
+                                            selection
+                                            options={categoryItems}
+                                            onChange={this.handleDropdownChange}
+                                        />
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <h3>Overall FIQ Points</h3>
+                                        <input
+                                            type="text"
+                                            name="questPoints"
+                                            placeholder="Points"
+                                            onChange={this.handleChange}
+                                        />
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Form>
+                    </Segment>
+                    {scenarios}
+                    <div className="subContainer" style={{ padding: '25px 0px', textAlign: 'right' }}>
+                        <Button
+                            icon
+                            labelPosition="left"
+                            className="teal"
+                            name="addScenario"
+                            onClick={this.onAddScenario}
+                        >
+                            <Icon name="add" size="large" />
+                            Add Scenarios
+                        </Button>
+                        <Button className="blue" name="createQuest" onClick={this.handleSubmit}>
+                            Create Quest
+                        </Button>
                     </div>
                 </div>
             </div>
--- client/src/components/quest/questScenarioCreation.js
+++ client/src/components/quest/questScenarioCreation.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Segment, Grid, Divider, Popup, TextArea } from 'semantic-ui-react';
+import { Segment, Grid, Divider, TextArea, Form, Accordion } from 'semantic-ui-react';
 
 class QuestScenarioCreation extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            checked: {},
-        };
+        this.state = {};
     }
 
     onSelectedChange = (event, index) => {
@@ -17,9 +15,6 @@ class QuestScenarioCreation extends React.Component {
             },
         }));
 
-        // event.target retrieves the label element instead of the checkbox element
-        // the checkbox element is directly before the label element in the DOM hierarchy,
-        // so attach .previousElementSibling to access it
         let checkbox = event.target.previousElementSibling;
         this.props.handleCheckboxChange(checkbox);
     };
@@ -32,66 +27,69 @@ class QuestScenarioCreation extends React.Component {
         this.setState({ activeIndex: newIndex });
     };
 
+    panels = [
+        {
+            key: 'details',
+            title: 'Add-on Details',
+            content: {
+                as: Form.TextArea,
+                label: 'Pros / Cons',
+                placeholder: 'Description',
+            },
+        },
+    ];
+
     render() {
         const number = this.props.scenarioNumber;
         const options = [];
-        const FIQoptions = [];
-        const timeOptions = [];
 
-        // Dynamically generate an array of option indices
         for (let i = 1; i < this.props.options + 1; i++) {
             options.push(i);
         }
 
-        // Prevents more than 1 checkbox from being checked at any point in time (customisable)
-        const { checked } = this.state;
-        const checkedCount = Object.keys(checked).filter((key) => checked[key]).length;
-        const disabled = checkedCount > 0;
-
         return (
             <div className="container" style={{ padding: '25px 0px' }}>
                 <Segment>
-                    <h2>Scenario {number}</h2>
-                    <Divider></Divider>
-                    <Grid columns="equal">
-                        <Grid.Column>
-                            <Popup content="Input your scenario here!" trigger={<h3>Scenario *</h3>} />
-                            <input
-                                type="text"
-                                name={'scenario' + number + 'name'}
-                                placeholder="Scenario"
-                                onChange={this.props.handleChange}
-                            />
-                        </Grid.Column>
-                    </Grid>
-                    <Grid>
-                        <Grid.Column>
-                            <TextArea
-                                name={'question' + number + 'explanation'}
-                                placeholder="Description"
-                                onChange={this.props.handleChange}
-                            />
-                            <h3>Options *</h3>
-                            <Grid>
-                                <Grid.Row columns={2}>
-                                    {options.map((value, index) => {
-                                        return (
-                                            <Grid.Column key={'options-' + number + '-' + value}>
-                                                <div className="field">
-                                                    <input
-                                                        type="text"
-                                                        name={'option-' + number + '-' + value}
-                                                        placeholder={'Option ' + value}
-                                                        onChange={this.props.handleChange}
-                                                    />
-                                                </div>
-                                            </Grid.Column>
-                                        );
-                                    })}
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
-                    </Grid>
+                    <Form>
+                        <h2>Scenario {number}</h2>
+                        <Divider></Divider>
+                        <Grid columns="equal">
+                            <Grid.Column>
+                                <h3>Title</h3>
+                                <input
+                                    type="text"
+                                    name={'scenario' + number + 'name'}
+                                    placeholder="Title"
+                                    onChange={this.props.handleChange}
+                                />
+                            </Grid.Column>
+                        </Grid>
+                        <h3>Options</h3>
+                        <Grid>
+                            <Grid.Row columns={2}>
+                                {options.map((value, index) => {
+                                    return (
+                                        <Grid.Column key={'options-' + number + '-' + value}>
+                                            <div className="field">
+                                                <input
+                                                    type="text"
+                                                    name={'option-' + number + '-' + value}
+                                                    placeholder={'Option ' + value}
+                                                    onChange={this.props.handleChange}
+                                                />
+                                                <Accordion
+                                                    as={Form.Field}
+                                                    style={{ padding: '20px 0px' }}
+                                                    panels={this.panels}
+                                                    onChange={this.props.handleChange}
+                                                />
+                                            </div>
+                                        </Grid.Column>
+                                    );
+                                })}
+                            </Grid.Row>
+                        </Grid>
+                    </Form>
                 </Segment>
             </div>
         );
