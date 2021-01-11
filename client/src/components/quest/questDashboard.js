c845c4d50a13338dc93e5509d9582130480fb04b
Gabriel-Lim01 <gabriellim01.18@ichat.sp.edu.sg>
2021-01-12 01:12:22 +0800

    fixed create new quest button redirect

--- client/src/components/quest/questDashboard.js
+++ client/src/components/quest/questDashboard.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Switch } from 'react-router-dom';
 import { Form, Button, Segment, Grid } from 'semantic-ui-react';
 import { containerStyle } from '../../common.js';
 import DashboardMenu from '../DashboardMenu.js';
+import verifyLogin from '../verifyLogin.js';
 
 class QuestDashboard extends React.Component {
     constructor(props) {
@@ -14,55 +15,64 @@ class QuestDashboard extends React.Component {
                 { icon: 'edit', color: 'blue', path: 'update' },
                 { icon: 'play', color: 'green', path: 'play' },
             ],
-            redirect: false,
+            redirect: null,
         };
     }
 
     redirectHandler = () => {
-        this.setState({ redirect: true });
-        this.renderRedirect();
+        this.setState({ redirect: '/quests/creation' });
     };
 
-    renderRedirect = () => {
-        if (this.state.redirect) {
-            return <Redirect to="/quests/creation" />;
-        } else console.log(this.state.redirect);
-    };
+    // renderRedirect = () => {
+    //     if (this.state.redirect) {
+    //         <Switch>
+    //             <Redirect to="/quests/creation" />
+    //         </Switch>
+    //     } else console.log(this.state.redirect);
+    // };
 
     //componentDidMount() {}
 
     render() {
-        return (
-            <div className="container" style={{ textAlign: 'left' }}>
-                <DashboardMenu page="quests"></DashboardMenu>
-                <div className="subContainer" style={containerStyle}>
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <h1>All Quests</h1>
-                        <button className="ui primary button" onClick={this.redirectHandler}>
-                            Create New<i className="right wrench icon"></i>
-                        </button>
-                    </div>
-                    <div className="ui stacked segment">
-                        <Form>
-                            <Segment color="green">
-                                <Grid.Row>
-                                    <h2>#1232: blah blah blah</h2>
-                                </Grid.Row>
-                                <Grid.Row style={{ display: 'flex', flexDirection: 'row-reverse' }}>
-                                    {this.state.functions.map((value, index2) => {
-                                        return (
-                                            <Grid.Column key={index2} style={{ display: 'flex' }}>
-                                                <Button circular icon={value.icon} color={value.color} />
-                                            </Grid.Column>
-                                        );
-                                    })}
-                                </Grid.Row>
-                            </Segment>
-                        </Form>
+        if (this.state.redirect) {
+            return <Redirect to={this.state.redirect} />
+        } else if (!verifyLogin()) {
+            return (
+                <h1>403 Forbidden</h1>
+            )
+        } else {
+            return (
+                <div className="container" style={{ textAlign: 'left' }}>
+                    <DashboardMenu page="quests"></DashboardMenu>
+                    <div className="subContainer" style={containerStyle}>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <h1>All Quests</h1>
+                            <button className="ui primary button" onClick={this.redirectHandler}>
+                                Create New<i className="right wrench icon"></i>
+                            </button>
+                        </div>
+                        <div className="ui stacked segment">
+                            <Form>
+                                <Segment color="green">
+                                    <Grid.Row>
+                                        <h2>#1232: blah blah blah</h2>
+                                    </Grid.Row>
+                                    <Grid.Row style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+                                        {this.state.functions.map((value, index2) => {
+                                            return (
+                                                <Grid.Column key={index2} style={{ display: 'flex' }}>
+                                                    <Button circular icon={value.icon} color={value.color} />
+                                                </Grid.Column>
+                                            );
+                                        })}
+                                    </Grid.Row>
+                                </Segment>
+                            </Form>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
